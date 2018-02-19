d40f114830af1c953b4e0b82029d6d18f9e0a1fd
Alexey Sarafannikov <alviksar@gmail.com>
2018-02-19 12:06:53 +0300

    Populate UI

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,10 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +18,25 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private Sandwich mSandwich;
+
+    // Sandwich detail TextViews
+    private TextView mAlsoKnownAs;
+    private TextView mIngredients;
+    private TextView mPlaceOfOrigin;
+    private TextView mDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        mAlsoKnownAs = findViewById(R.id.also_known_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
+        mPlaceOfOrigin = findViewById(R.id.origin_tv);
+        mDescription = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,8 +52,8 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
+        mSandwich  = JsonUtils.parseSandwichJson(json);
+        if (mSandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
@@ -45,10 +61,10 @@ public class DetailActivity extends AppCompatActivity {
 
         populateUI();
         Picasso.with(this)
-                .load(sandwich.getImage())
+                .load(mSandwich.getImage())
                 .into(ingredientsIv);
 
-        setTitle(sandwich.getMainName());
+        setTitle(mSandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -58,5 +74,29 @@ public class DetailActivity extends AppCompatActivity {
 
     private void populateUI() {
 
+        if (mSandwich == null) {
+            closeOnError();
+            return;
+        }
+
+        mPlaceOfOrigin.setText(mSandwich.getPlaceOfOrigin());
+        mDescription.setText(mSandwich.getDescription());
+
+        // Make list of names
+        if (mSandwich.getAlsoKnownAs() != null){
+            mAlsoKnownAs.setText("");
+            for (String name: mSandwich.getAlsoKnownAs()
+                    ) {
+                mAlsoKnownAs.append(name);
+            }
+        }
+        // Make list of ingredients
+        if (mSandwich.getIngredients() != null){
+            mIngredients.setText("");
+            for (String ingredient: mSandwich.getIngredients()
+                 ) {
+                mIngredients.append(ingredient);
+            }
+        }
     }
 }
